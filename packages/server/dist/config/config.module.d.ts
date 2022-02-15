7fa24c03a9fea8ce94a31c41c37d4684d776a71e
Paul Fidika <paul@fidika.com>
2022-02-15 11:13:28 -0700

    remove git tracking dist folder

--- packages/server/dist/config/config.module.d.ts
+++
@@ -1,8 +0,0 @@
-import { DynamicModule } from '@nestjs/common';
-export interface ConfigModuleOptions {
-    path?: string;
-    isGlobal?: boolean;
-}
-export declare class ConfigModule {
-    static forRoot(options?: ConfigModuleOptions): DynamicModule;
-}
