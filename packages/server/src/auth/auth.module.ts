4067a7a7f43263618bb18de79a63f545d098d156
adrianjmejias <adrianjmejias@gmail.com>
2022-03-28 18:11:32 +0100

    feat(server): verifying tokens in the server

--- packages/server/src/auth/auth.module.ts
+++ packages/server/src/auth/auth.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  providers: [AuthService]
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
+
 export class AuthModule {}
